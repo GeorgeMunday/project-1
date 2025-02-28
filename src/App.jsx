845a3b8b870b8f4a9258c8a458603d228dadaab0
John Doe <[email protected]>
2025-02-28 15:49:09 +0000

    start

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <header>
+      <div>
+      <a href ="">yay</a> 
+      <a href ="">yay</a>
+      
+      </div>
+    </header>
+    </>
+  )
+}
+
+export default App
